7f05b14fe0a8ba6ffbd2a0cfa141b78b5090f51a
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-12-01 14:54:56 +0100

    modify fetch(alert error)

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -16,20 +16,26 @@ const WeatherBox = props => {
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0901dac43ab2dd08220f06d64d1a0920&units=metric`)
     
-   .then(res => res.json())
-   .then(data => {
+    .then(res => {
+      if(res.status === 200) {
+        return res.json()
+          .then(data => {
 
-      const weatherData = {
-        city: data.name,
-        temp: data.main.temp,
-        icon: data.weather[0].icon,
-        description: data.weather[0].main
-      };
-      setPending(false)
-      setWeather(weatherData)
-      console.log(weatherData);
-   });
-
+            const weatherData = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main
+            };
+      
+            setPending(false)
+            setWeather(weatherData)
+            console.log(weatherData);
+          })
+        } else {
+          alert('ERROR!')
+        }
+    })
   }); 
 
   return (
